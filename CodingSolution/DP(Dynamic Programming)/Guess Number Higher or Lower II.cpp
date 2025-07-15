94481b775754ddd996fe2778b286532acc1ab79b
Mohammad Ayan Siddiqui <ayansiddiquiktmo@gmail.com>
2025-07-15 23:02:03 +0530

    Create Guess Number Higher or Lower II.cpp

---
+++ CodingSolution/DP(Dynamic Programming)/Guess Number Higher or Lower II.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+//using memoization
+    int solve(int start,int end,vector<vector<int>> &dp)
+    {
+        if(start>=end)
+        {
+            return 0;
+        }
+        if(dp[start][end]!=-1)
+        {
+            return dp[start][end];
+        }
+        int maxi=INT_MAX;
+        for(int i=start;i<=end;i++)
+        {
+            maxi=min(maxi,i+max(solve(start,i-1,dp),solve(i+1,end,dp)));
+        }
+        return dp[start][end]=maxi;
+    }
+    int getMoneyAmount(int n) {
+        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+        return solve(1,n,dp);
+    }
+
+**************************************************************************************************************************************************
+//using tabulation
+    int getMoneyAmount(int n) {
+        vector<vector<int>> dp(n+2,vector<int>(n+2,0));
+        for(int start=n;start>0;start--)
+        {
+            for(int end=start;end<=n;end++)
+            {
+                if(start==end)
+                {
+                    continue;
+                }
+                else
+                {
+                     int maxi=INT_MAX;
+                     for(int i=start;i<=end;i++)
+                     {
+                        maxi=min(maxi,i+max(dp[start][i-1],dp[i+1][end]));
+                     }
+                      dp[start][end]=maxi;
+                }
+            }
+        }
+        return dp[1][n];
+    }
+};
